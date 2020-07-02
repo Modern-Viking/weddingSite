ffefab465bbcf193d9f191f902427b33c368a1fb
Modern Viking <thomascustoms715@gmail.com>
2020-07-02 10:24:54 -0600

    updated image file names

--- src/Components/Photosbody/index.js
+++ src/Components/Photosbody/index.js
@@ -8,13 +8,13 @@ const Weddingbody = () => {
 
         <div className="column is-one-quarter">
             <div className="">
-                <img alt="in our masks" src="https://scontent-den4-1.xx.fbcdn.net/v/t1.0-9/93914725_10158518138948648_2269460306889539584_o.jpg?_nc_cat=110&_nc_sid=110474&_nc_ohc=29xSdK7-bG0AX_ZTzEV&_nc_ht=scontent-den4-1.xx&oh=041509cd85da7841017956071a4255cf&oe=5EE4C386"/>
+                <img alt="black and white" src="../../Images/cleanEngagments/sqr-blk-wht.jpg"/>
             </div>
         </div>
 
             <div className="column is-one-quarter">
             <div className="">
-                <img alt="at the salt flats" src="https://scontent-den4-1.xx.fbcdn.net/v/t1.0-9/90327689_10222427213331778_2539823902557732864_o.jpg?_nc_cat=108&_nc_sid=8024bb&_nc_ohc=FxJc5OL7RngAX_TPapX&_nc_ht=scontent-den4-1.xx&oh=f49130e6837484bc2f8fecd38cc6c3c1&oe=5EE3BE03"/>
+                <img alt="love in the meadow" src="../../Images/cleanEngagments/option2.jpg"/>
             </div> 
         </div>
 
