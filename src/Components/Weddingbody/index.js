7ef6eb358102cd981944c9bf9af4ddbcbca0ff51
Modern Viking <thomascustoms715@gmail.com>
2020-09-26 19:12:04 -0600

    updated info on main page for receptions

--- src/Components/Weddingbody/index.js
+++ src/Components/Weddingbody/index.js
@@ -16,6 +16,15 @@ const Weddingbody = () => {
     </h2>
     </div>
     </div>
+    <br/>
+    <div className="columns">
+      <div className="column">
+        <h1 className="subtitle">
+          We regret to inform everyone that due to Covid-19 restrictions both receptions have been canceled.
+          While we would have loved to see and celebrate with all of you we feel it is in the best interest and health of everyone to cancel the receptions.
+        </h1>
+      </div>
+    </div>
     <div className="columns">
     <div className="column">
     <h1 className="title">
@@ -33,14 +42,6 @@ const Weddingbody = () => {
       <s>October 24, 2020 in Redlands</s>
     </h2>
     </div>
-    </div>
-    <br/>
-    <div className="columns">
-      <div className="column">
-        <h1 className="subtitle">
-          More information will be available as it is decided
-        </h1>
-      </div>
     </div>
   </div>
 </div>
