ff9dd13aa1196e4e13cfd1afedcd429b1761f0d3
Modern Viking <thomascustoms715@gmail.com>
2020-09-26 19:08:13 -0600

    crossed out dates for receptions

--- src/Components/Weddingbody/index.js
+++ src/Components/Weddingbody/index.js
@@ -19,10 +19,10 @@ const Weddingbody = () => {
     <div className="columns">
     <div className="column">
     <h1 className="title">
-      <s>Utah Reception</s>
+      Utah Reception
     </h1>
     <h2 className="subtitle">
-      October 17, 2020 in Salt Lake City
+      <s>October 17, 2020 in Salt Lake City</s>
     </h2>
     </div>
     <div className="column">
@@ -30,11 +30,18 @@ const Weddingbody = () => {
       California Reception
     </h1>
     <h2 className="subtitle">
-      October 24, 2020 in Redlands
+      <s>October 24, 2020 in Redlands</s>
     </h2>
     </div>
     </div>
     <br/>
+    <div className="columns">
+      <div className="column">
+        <h1 className="subtitle">
+          More information will be available as it is decided
+        </h1>
+      </div>
+    </div>
   </div>
 </div>
 </section>
