a3e6325350a7229d693a0212a59ab2a1a998b321
Modern Viking <thomascustoms715@gmail.com>
2020-10-03 21:45:50 -0600

    updating photos

--- src/Components/Photosbody/index.js
+++ src/Components/Photosbody/index.js
@@ -8,61 +8,61 @@ const Weddingbody = () => {
 
         <div className="column is-one-quarter">
             <div className="">
-                <img alt="black and white" src="https://github.com/Modern-Viking/weddingSite/blob/master/src/Images/clean%20wedding%20photos/Nick%20&%20Mary%20(22%20of%2075).jpg?raw=true"/>
+                <img alt="black and white" src="https://github.com/Modern-Viking/weddingSite/blob/master/src/Images/clean%20wedding%20photos/Nick%20and%20Mary%20(12%20of%2089).jpg?raw=true"/>
             </div>
         </div>
 
             <div className="column is-one-quarter">
             <div className="">
-                <img alt="love in the meadow" src="https://github.com/Modern-Viking/weddingSite/blob/master/src/Images/cleanEngagments/option2.jpg?raw=true"/>
+                <img alt="love in the meadow" src=""/>
             </div> 
         </div>
 
         <div className="column is-one-quarter">
             <div className="">
-                <img alt="love in the meadow" src="https://github.com/Modern-Viking/weddingSite/blob/master/src/Images/cleanEngagments/583A3949-130.jpg?raw=true"/>
+                <img alt="love in the meadow" src=""/>
             </div> 
         </div>
 
         <div className="column is-one-quarter">
             <div className="">
-                <img alt="love in the meadow" src="https://github.com/Modern-Viking/weddingSite/blob/master/src/Images/cleanEngagments/583A3957-127.jpg?raw=true"/>
+                <img alt="love in the meadow" src=""/>
             </div> 
         </div>
 
         <div className="column is-one-quarter">
             <div className="">
-                <img alt="love in the meadow" src="https://github.com/Modern-Viking/weddingSite/blob/master/src/Images/cleanEngagments/583A4045-117.jpg?raw=true"/>
+                <img alt="love in the meadow" src=""/>
             </div> 
         </div>
 
         <div className="column is-one-quarter">
             <div className="">
-                <img alt="love in the meadow" src="https://github.com/Modern-Viking/weddingSite/blob/master/src/Images/cleanEngagments/583A4187-91.jpg?raw=true"/>
+                <img alt="love in the meadow" src=""/>
             </div> 
         </div>
 
         <div className="column is-one-quarter">
             <div className="">
-                <img alt="love in the meadow" src="https://github.com/Modern-Viking/weddingSite/blob/master/src/Images/cleanEngagments/583A4213-83.jpg?raw=true"/>
+                <img alt="love in the meadow" src=""/>
             </div> 
         </div>
 
          <div className="column is-one-quarter">
             <div className="">
-                <img alt="love in the meadow" src="https://github.com/Modern-Viking/weddingSite/blob/master/src/Images/cleanEngagments/option3.jpg?raw=true"/>
+                <img alt="love in the meadow" src=""/>
             </div> 
         </div>
 
         <div className="column is-one-quarter">
             <div className="">
-                <img alt="love in the meadow" src="https://github.com/Modern-Viking/weddingSite/blob/master/src/Images/cleanEngagments/option4.jpg?raw=true"/>
+                <img alt="love in the meadow" src=""/>
             </div> 
         </div>
 
         <div className="column is-one-quarter">
             <div className="">
-                <img alt="love in the meadow" src="https://github.com/Modern-Viking/weddingSite/blob/master/src/Images/cleanEngagments/583A4421-44.jpg?raw=true"/>
+                <img alt="love in the meadow" src=""/>
             </div> 
         </div>
 
